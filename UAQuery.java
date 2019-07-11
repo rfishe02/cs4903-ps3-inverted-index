8e798844bf922f1d6873f4cd62499129f78e0499
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-07-10 22:16:46 -0500

    development of query function

--- UAQuery.java
+++ UAQuery.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Comparator;
 import java.util.ArrayList;
@@ -22,7 +23,7 @@ public class UAQuery {
 
   public static void main(String[] args) {
 
-    String[] test = {"youtube"};
+    String[] test = {"xxxxx"};
 
     runQuery(test);
 
@@ -31,18 +32,8 @@ public class UAQuery {
   public static String[] runQuery(String[] query) {
 
     String filename;
-    String record;
-    int count;
-    int start;
-    int docID;
-    float rtfIDF;
-    int i;
 
     try {
-
-      RandomAccessFile dict = new RandomAccessFile("output/dict.raf","rw");
-      RandomAccessFile post = new RandomAccessFile("output/post.raf","rw");
-      RandomAccessFile map = new RandomAccessFile("output/map.raf","rw");
 
       /*
       Build a document term matrix, then use RTFIDF to calculate cosine similarity.
@@ -54,47 +45,12 @@ public class UAQuery {
 
       HashMap<Integer,Integer> docMap = new HashMap<>();
       HashMap<String,Integer> termMap = new HashMap<>();
-      HashSet<String> files = new HashSet<>();
-      int row = 0;
-      int col = 0;
 
-      for(String s : query) {
+      mapRowsCols(docMap,termMap,query);
 
-        if(!termMap.containsKey(s)) {
-          termMap.put(s,row);
-          row++;
-        } // Map terms to rows in the document term matrix.
+      System.out.println(termMap.size());
 
-        i = 0;
-        do {
-          dict.seek(hash(s,i) * (DICT_LEN+2));
-          record = dict.readUTF();
-          i++;
-        } while(record.trim().compareTo(s) != 0); // Find the term in the dictionary.
-
-        count = dict.readInt();
-        start = dict.readInt();
-
-        post.seek(((start-count)+1) * POST_LEN);
-        for(int x = 0; x < count; x++) {
-          docID = post.readInt();
-          rtfIDF = post.readFloat();
-
-          if(!docMap.containsKey(docID)) {
-            docMap.put(docID,col);
-            col++;
-          } // Map document ID to column.
-
-          //map.seek(docID * (DOC_LEN + 2));
-          //filename = map.readUTF();
-          // Collect the file names.
-
-        } // Read each posting listed.
-
-      }
-
-      dict.close();
-      post.close();
+      RandomAccessFile map = new RandomAccessFile("output/map.raf","rw");
       map.close();
 
     } catch(IOException ex) {
@@ -105,11 +61,57 @@ public class UAQuery {
     return null;
   }
 
+  public static void mapRowsCols(HashMap<Integer,Integer> docMap, HashMap<String,Integer> termMap, String[] query) throws IOException {
+    RandomAccessFile dict = new RandomAccessFile("output/dict.raf","rw");
+    RandomAccessFile post = new RandomAccessFile("output/post.raf","rw");
+    String record;
+    int row = 0;
+    int col = 0;
+    int count;
+    int start;
+    int docID;
+    int i;
+
+    for(String s : query) {
+      i = 0;
+      do {
+        dict.seek(hash(s,i) * (DICT_LEN+2));
+        record = dict.readUTF();
+        i++;
+      } while(record.trim().compareTo("NA") != 0 && record.trim().compareTo(s) != 0); // Find the term in the dictionary.
+
+      if(record.trim().compareTo("NA") != 0) {
+        if(!termMap.containsKey(s)) {
+          termMap.put(s,row);
+          row++;
+        } // Map terms to rows in the document term matrix.
+
+        count = dict.readInt();
+        start = dict.readInt();
+
+        post.seek(((start-count)+1) * POST_LEN);
+        for(int x = 0; x < count; x++) {
+          docID = post.readInt();
+          post.readFloat(); // rtfIDF
+
+          if(!docMap.containsKey(docID)) {
+            docMap.put(docID,col);
+            col++;
+          } // Map document ID to column.
+
+        } // Read each posting listed.
+      }
+    } // Map terms & documets to columns.
+
+    dict.close();
+    post.close();
+
+  }
+
   public static float[][] documentTermMatrix(HashMap<String,Integer> docMap, HashMap<String,Integer> termMap, HashSet<String> files, int row, int col) {
 
     BufferedReader br;
     String read;
-    float[][] res = new float[size][size];
 
     /*
     try {
@@ -136,7 +138,7 @@ public class UAQuery {
       System.exit(1);
     }*/
 
-    return res;
+    return null;
 
   }
 
