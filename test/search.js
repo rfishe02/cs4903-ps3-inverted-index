ed2780239cb5125abcfba2686d22cbf9ed52f4bb
rfishe02 <rfishe02@g.uafs.edu>
2019-07-17 23:55:30 -0500

    building interface

---
+++ test/search.js
@@ -0,0 +1,37 @@
+
+// https://blog.teamtreehouse.com/create-ajax-contact-form
+
+$(function() {
+    
+    var form = $('#ajax-search');
+    var formMessages = $('#test');
+    
+    $(form).submit(function(event) {
+        event.preventDefault(); // Stop the browser from submitting the form.
+        var formData = $(form).serialize();
+        
+        $.ajax({
+            
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+            
+        }).done(function(response) {
+
+            $(formMessages).text(response);
+            $('#search-box').val('');
+            
+        }).fail(function(data) {
+            
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occured and your query could not be sent.');
+            }
+            
+        }); // Submit the form using AJAX.
+        
+    }); // Set up an event listener for the contact form.
+
+});
+
---
+++ test/search.php
@@ -0,0 +1,37 @@
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+        // Get the form fields and remove whitespace.
+        //$name = strip_tags(trim($_POST["name"]));
+        //$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        //$email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        
+        $text = trim($_POST["search-box"]);
+
+        if ( empty($text) ) {
+            http_response_code(400); // Set a 400 (bad request) response code and exit.
+            echo "Oops! There was a problem with your submission.";
+            exit;
+        } // Check that data was sent to the engine.
+
+        //fopen('./output/dict.raf', 'r');
+        
+        echo "Success!"
+       
+        // Send the search.
+        /*
+        if (  ) {
+            http_response_code(200); // Set a 200 (okay) response code.
+            echo "Thank You! Your message has been sent.";
+        } else {
+            http_response_code(500); // Set a 500 (internal server error) response code.
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }*/
+
+    } else {
+        http_response_code(403); // Not a POST request, set a 403 (forbidden) response code.
+        echo "Oops! There was a problem with your submission.";
+    }
+
+?>
