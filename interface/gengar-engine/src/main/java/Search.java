50a9dd303e55c89f7efa19b846b716f15bb4b04f
rfishe02 <rfishe02@g.uafs.edu>
2019-07-18 13:19:00 -0500

    using tomcat to build interface

--- interface/gengar-engine/src/main/java/Search.java
+++ interface/gengar-engine/src/main/java/Search.java
@@ -7,27 +7,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class DataServ extends HttpServlet {
-
-   private String message;
+//@WebServlet(name = "Search", urlPatterns = "/Search")
+public class Search extends HttpServlet {
 
    public void init() throws ServletException {
-      // Do required initialization
-      message = "Hello World";
+   
    }
 
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
       // Set response content type
       response.setContentType("text/html");
 
       // Actual logic goes here.
       PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
+      out.println("<h1>test</h1>");
+      out.close();
+      
+   }
+   
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   
    }
 
    public void destroy() {
-      // do nothing.
+   
    }
 }
