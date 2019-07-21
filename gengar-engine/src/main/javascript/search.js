3ecf0256e08a8044a52eee7ccc06f488529dc8c6
rfishe02 <rfishe02@g.uafs.edu>
2019-07-21 18:13:20 -0500

    better interface design

--- gengar-engine/src/main/javascript/search.js
+++ gengar-engine/src/main/javascript/search.js
@@ -20,7 +20,7 @@ $(function() {
             
             if(links.length > 1) {
                 
-                $('#result').append("<div div class='row justify-content-start align-items-center h-100' id ='result-row'>");
+                $('#result').append("<div div class='row justify-content-start align-items-center mt-4 h-100' id ='result-row'>");
              
                 for(a = 1; a < links.length; a++) {
                     
@@ -32,7 +32,7 @@ $(function() {
                     }
                     
                     if(out.length > 0) {
-                        $('#result-row').append("<div class='col-12 mb-4'><div class='card h-100'><div class='card-body'><br><h5 class='card-title'><a href='"+links[0]+"/"+text[0]+"'>"+text[0]+"</a></h5><p class='card-text'>"+out+"</p><br></div></div></div>");
+                        $('#result-row').append("<div class='col-12 mb-4'><div class='card h-100 shadow-sm'><div class='card-body'><br><h5 class='card-title'><a href='"+links[0]+"/"+text[0]+"'>"+text[0]+"</a></h5><p class='card-text'>"+out+"</p><br></div></div></div>");
                     }
    
                 }
@@ -40,7 +40,7 @@ $(function() {
                 $('#result-row').append("</div>");
                 
             } else {
-                $('#result').append("<div div class='row justify-content-center align-items-center h-100'><div class='col-12 mb-5'><p class='display-2 text-muted text-center'>"+response+"</p></div></div>");  
+                $('#result').append("<div div class='row justify-content-center align-items-center mt-4 h-100'><div class='col-12 mb-5' style='padding-top: 115px;'><p class='display-2 text-muted text-center'>"+response+"</p></div></div>");  
             }
             
             $('#search-box').val('');
